72024e1cb44b40660e8f9c4b01006f501178752e
Jonathan Martin <me@nybblr.com>
2016-01-16 18:59:30 -0500

    Bootstrap.

---
+++ README.md
@@ -0,0 +1,1 @@
+# markdown-it-ghost-upload [![npm version](http://img.shields.io/npm/v/markdown-it-ghost-upload.svg?style=flat-square)](https://www.npmjs.org/package/markdown-it-ghost-upload)
---
+++ test.js
@@ -0,0 +1,8 @@
+import { equal } from 'assert'
+import md from 'markdown-it'
+import ghostUpload from './'
+
+// equal(
+//   md().use(ghostUpload).render('![]()'),
+//   '<h1 id="h1">H1</h1>\n<h2 id="h2">H2</h2>\n'
+// )
